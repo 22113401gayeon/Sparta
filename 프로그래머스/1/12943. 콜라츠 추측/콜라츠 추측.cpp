5d5d00e23465a2ff89b9803c9ec0b2c6d751e9f8
김가연 <hipink2512@naver.com>
2025-09-02 15:14:26 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(int n) {
+    long long num = n; 
+    int count = 0;
+
+    while (num != 1) {
+        if (count == 500) return -1;
+
+        if (num % 2 == 0) num /= 2;
+        else num = num * 3 + 1;
+
+        count++;
+    }
+
+    return count;
+}
